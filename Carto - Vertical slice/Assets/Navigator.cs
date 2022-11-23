fd5c370003336e35ceaa22292b640c42d418c8fd
j5x <32881@ma-web.nl>
2022-11-23 16:16:28 +0100

    Added random NPC movement.

---
+++ Carto - Vertical slice/Assets/Navigator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigator : MonoBehaviour
+{
+    public GameObject[] Waypoints;
+    public int speed;
+    public int currentWaypoint;
+    public float WPradius = .5f;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Waypoints[currentWaypoint].transform.position, transform.position) < WPradius)
+        {
+            currentWaypoint = (int)Mathf.Floor(Random.Range(0,3));
+
+        }
+        transform.position = Vector3.MoveTowards(transform.position, Waypoints[currentWaypoint].transform.position, speed * Time.deltaTime);
+    }
+}
+
