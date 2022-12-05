370a59789da05363e4ea40400942fa9c76281cd9
10KJVN <Jahva_game11@hotmail.com>
2022-12-05 11:59:08 +0100

    Begin aan Controls, scripts wip.

---
+++ MyCartoWorkspace/Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [Header("UI")]
+    public Image image;
+
+    //Drag and drop
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+        image.raycastTarget = false;
+        //parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition; 
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+        image.raycastTarget = true;
+        //transform.SetParent(parentAfterDrag);
+    }
+}
