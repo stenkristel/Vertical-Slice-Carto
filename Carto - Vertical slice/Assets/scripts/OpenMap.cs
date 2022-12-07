b7839ea101469aabb0f2d735bcac66a82c4cf45b
stenkristel <stenkristel2016@gmail.com>
2022-12-07 09:12:27 +0100

    begin UI fade in

--- Carto - Vertical slice/Assets/scripts/OpenMap.cs
+++ Carto - Vertical slice/Assets/scripts/OpenMap.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class camarafollow : MonoBehaviour
+public class OpenMap : MonoBehaviour
 {
     private Vector3 startpos = new Vector3(-1, 12, -9);
     private Vector3 endpos = new Vector3(-1, 162, -141);
@@ -10,6 +11,9 @@ public class camarafollow : MonoBehaviour
     private bool zoomingout;
     private bool zoomingin;
     [SerializeField] private float speed;
+
+    private GameObject cam;
+    private GameObject canvas;
 
     private void Start()
     {
@@ -35,14 +39,23 @@ public class camarafollow : MonoBehaviour
             }
         }
 
-        
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(fade());
+        }
 
+            camerazoomout();
+
+
+    }
+
+    private void camerazoomout()
+    {
         if (zoomingin == true)
         {
             //camerazoomout(-speed, speed);
-            gameObject.transform.position += new Vector3(0, -speed, speed) * Time.deltaTime;
-            if (gameObject.transform.position.y <= startpos.y)
+            cam.transform.position += new Vector3(0, -speed, speed) * Time.deltaTime;
+            if (cam.transform.position.y <= startpos.y)
             {
                 zoomingin = false;
                 mapmode = false;
@@ -52,8 +65,8 @@ public class camarafollow : MonoBehaviour
         if (zoomingout == true)
         {
             //camerazoomout(speed, -speed);
-            gameObject.transform.position += new Vector3(0, speed, -speed) * Time.deltaTime;
-            if (gameObject.transform.position.y >= endpos.y)
+            cam.transform.position += new Vector3(0, speed, -speed) * Time.deltaTime;
+            if (cam.transform.position.y >= endpos.y)
             {
                 zoomingout = false;
                 mapmode = true;
@@ -61,24 +74,21 @@ public class camarafollow : MonoBehaviour
         }
     }
 
+    private void UIfade()
+    {
+        foreach (Transform child in canvas.transform)
+        {
+            if (child.tag == "map")
+            {
+                RawImage Image = child.GetComponent<RawImage>();
+                Image.color = new Color(Image.color.r, Image.color.g, Image.color.b, 0.5f);
+            }
+        }
+    }
 
-    //private void camerazoomout(float yspeed, float zspeed)
-    //{
-        //gameObject.transform.position += new Vector3(0, yspeed, zspeed) * Time.deltaTime;
-        //gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, endpos, speed);
-        //if (gameObject.transform.position.y <= startpos.y)
-        //{
-            //zoomingin = false;
-            //mapmode = false;
-        //}
-        //else if (gameObject.transform.position.y >= endpos.y)
-        //{
-            //zoomingout = false;
-            //mapmode = true;
-        //}
-        //else
-        //{
-            //camerazoomout(yspeed, zspeed);
-        //}
-    //}
+    private IEnumerator fade(RawImage image)
+    {
+        image.color = new Color(image.color.r, image.color.g, image.color.b, -0.1f);
+        yield return new WaitForSeconds(1); 
+    }
 }
