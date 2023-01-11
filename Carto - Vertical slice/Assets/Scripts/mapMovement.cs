09f87ce641540ae821e399508b243e6f695a1236
10KJVN <Jahva_game11@hotmail.com>
2023-01-11 13:00:26 +0100

    Fixed map movement, map piece moves to center position, working scope. ready to pull map pieces, WIP pick up system.

--- Carto - Vertical slice/Assets/Scripts/mapMovement.cs
+++ Carto - Vertical slice/Assets/Scripts/mapMovement.cs
@@ -16,19 +16,19 @@ public class mapMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.position += new Vector3(0, 200, 0);
+            transform.position += new Vector3(0, 1, 0);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            transform.position -= new Vector3(0, 200, 0);
+            transform.position -= new Vector3(0, 1, 0);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position -= new Vector3(200, 0, 0);
+            transform.position -= new Vector3(5, 0, 0);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(200, 0, 0);
+            transform.position += new Vector3(5, 0, 0);
         }
     }
 }
