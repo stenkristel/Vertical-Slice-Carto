3e099e0866d20efa9651e77f42a86329e2daffa7
j5x <32881@ma-web.nl>
2023-01-11 13:01:19 +0100

    npc animation script added

--- Carto - Vertical slice/Assets/Scripts/Navigator.cs
+++ Carto - Vertical slice/Assets/Scripts/Navigator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Navigator : MonoBehaviour
 {
+    private Animator animator;
     public GameObject[] Waypoints;
     public int speed;
     public int speedafterfreeze;
@@ -14,7 +15,7 @@ public class Navigator : MonoBehaviour
 
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,12 +24,16 @@ public class Navigator : MonoBehaviour
         if (Vector3.Distance(Waypoints[currentWaypoint].transform.position, transform.position) < WPradius)
         {
             currentWaypoint = (int)Mathf.Floor(Random.Range(0,3));
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isIdle", false);
 
         }
         transform.position = Vector3.MoveTowards(transform.position, Waypoints[currentWaypoint].transform.position, speed * Time.deltaTime);
         if (currentWaypoint == 2)
         {
             StartCoroutine(Freeze());
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isWalking", false);
         }
     }
 
